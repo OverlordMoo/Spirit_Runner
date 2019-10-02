67d72112f8e1ccf96f487b2cc56b4d65320a019f
Leevi Junttila <leevi.junttila@me.com>
2019-10-02 16:09:25 +0300

    added project to repo
    initial project commit

---
+++ Spirit Runner/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<GameObject> map;
+    public List<GameObject> pieces;
+
+    public int visiblePieces;
+    public int pastPieces;
+    public GameObject piecePrefab1;
+    public GameObject piecePrefab2;
+    public GameObject piecePrefab3;
+
+    private int numOfPieceOptions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+
+            map.Add(child.gameObject);
+        }
+
+        pieces.Add(piecePrefab1);
+        pieces.Add(piecePrefab2);
+        pieces.Add(piecePrefab3);
+        numOfPieceOptions = pieces.Count -2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(map.Count < visiblePieces)
+        {
+            GenerateMap();
+        }
+        //RemovePastMap();
+    }
+
+    void GenerateMap()
+    {
+        //Create random for choosing the next piece
+        System.Random rnd = new System.Random();
+        int rInt = rnd.Next(0, numOfPieceOptions);
+        //GameObject piece = pieces[rInt];
+
+        GameObject last = map[map.Count - 1];                          //Last = the last generated piece
+        Vector3 newZ = new Vector3(0, 0, 12f);                        //next piece should be generated Z+6.5F
+        Vector3 newLocation = last.transform.position + newZ;          //Set new location for next piece
+
+        GameObject nextPiece = Instantiate(pieces[rInt], newLocation, Quaternion.identity);    //Instantiate new piece
+        nextPiece.transform.Rotate(-90f, 0f, 0f, Space.Self);
+        map.Add(nextPiece);                                         //Put the new piece to the list
+        transform.parent = gameObject.transform;                    //Put new piece under Generator object
+    }
+
+
+    void RemovePastMap()
+    {
+
+    }
+}
---
+++ Spirit Runner/Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+    public bool sliding;
+    public Collider PlayerColl;
+    //notifies parent on collision
+
+    private void Start()
+    {
+        sliding = false;
+        PlayerColl = gameObject.GetComponent<BoxCollider>();
+    }
+    public void SetSlideFalse()
+    {
+        sliding = false;
+    }
+    public void SetSlideTrue()
+    {
+        sliding = true;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //call a function from the player controllers movement script depeding on what the body collides with
+        if (collision.other.CompareTag("Fence") && sliding == true)
+        {
+            Physics.IgnoreCollision(collision.collider, PlayerColl);
+        }
+        if (collision.other.CompareTag("Wall") || collision.other.CompareTag("Fence") && sliding==false)
+        {
+            transform.parent.GetComponent<PlayerMovement>().WallCollisionDetected(this);
+        }
+        if (collision.other.CompareTag("Base"))
+        {
+            transform.parent.GetComponent<PlayerMovement>().JumpLanded(this);
+        }
+        if(collision.other.CompareTag("RedEnemy"))                                              //Tag is for demo. See if hit red enemy.
+        {
+            if(transform.parent.GetComponent<ShapeShifting>().RedOn == true)                    //check if red shape is active from ShapeShifting.cs
+            {
+                Destroy(collision.gameObject, 0);                                               //if red active destroy enemy
+            }
+        }
+        if (collision.other.CompareTag("BlueEnemy"))
+        {
+            if (transform.parent.GetComponent<ShapeShifting>().BlueOn == true)                  //same thinn as red except different colour
+            {
+                Destroy(collision.gameObject, 0);
+            }
+        }
+        if (collision.other.CompareTag("BlackEnemy"))
+        {
+            if (transform.parent.GetComponent<ShapeShifting>().BlackOn == true)                 //same thinn as red except different colour
+            {
+                Destroy(collision.gameObject, 0);
+            }
+        }
+        if (collision.other.CompareTag("Collectible"))                                          // if tag is collectible
+        {
+            Destroy(collision.gameObject, 0);                                                   //destroys collectible
+            if (transform.parent.GetComponent<ShapeShifting>().ShapeOn == true)                  //if any shape is active                             
+            {
+                transform.parent.GetComponent<ShapeShifting>().collectiblePicked = true;         //collectible is picked, value is used in shapeshifting.cs
+            }
+        }
+    }
+}
