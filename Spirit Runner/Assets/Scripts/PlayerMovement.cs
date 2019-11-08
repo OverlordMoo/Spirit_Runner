975539ccac50fb8fabb030b7db594514977322cf
JoelMarttala <jokke.marttala@gmail.com>
2019-11-08 12:16:54 +0200

    Fixing update
    This is yet another attempt to fix master and return reich back to its glory

--- Spirit Runner/Assets/Scripts/PlayerMovement.cs
+++ Spirit Runner/Assets/Scripts/PlayerMovement.cs
@@ -41,119 +41,101 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    IEnumerator Fly()
-    {
-        
-        flying = true;
-        
-        flyPos = new Vector3(transform.position.x, transform.position.y + flyHeight , transform.position.z + 1);
-        PlayerRigidBody.useGravity = false;
-        
-        while (transform.position.y < flyPos.y)
+
+        IEnumerator Fly()
         {
-            transform.Translate(Vector3.up*takeOffSpeed*Time.deltaTime);
-            yield return new WaitForEndOfFrame();
-        }
-        yield return new WaitForSeconds(flyTime);
-        PlayerRigidBody.useGravity = true;
-        flying = false;
-        
-        
 
-    IEnumerator Fly()
-    {
+            flying = true;
 
-        flying = true;
+            flyPos = new Vector3(transform.position.x, transform.position.y + flyHeight, transform.position.z);
+            PlayerRigidBody.useGravity = false;
 
-        flyPos = new Vector3(transform.position.x, transform.position.y + flyHeight, transform.position.z);
-        PlayerRigidBody.useGravity = false;
-
-        while (transform.position.y < flyPos.y)
-        {
-            transform.Translate(Vector3.up * takeOffSpeed * Time.deltaTime);
-            yield return new WaitForEndOfFrame();
-        }
-        yield return new WaitForSeconds(flyTime);
-        landPos = new Vector3(transform.position.x, transform.position.y - flyHeight, transform.position.z);
-        while (transform.position.y > landPos.y)
-        {
-            transform.Translate(Vector3.down * takeOffSpeed * Time.deltaTime);
-            yield return new WaitForEndOfFrame();
-        }
-        PlayerRigidBody.useGravity = true;
-        flying = false;
-
-
-    }
-
-
-    IEnumerator GameOver()
-    {
-        yield return new WaitForSeconds(exitTime);
-        SceneManager.LoadScene(1);
-
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        movement = new Vector3(0,0,1);
-        PlayerRigidBody = PlayerBody.GetComponent<Rigidbody>();
-        playerColl = PlayerBody.GetComponent<PlayerCollisionDetection>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (PlayerRigidBody.velocity.y>=0)
-        {
-            playerAnim.SetBool("Going_up", true);
-        }
-        if (PlayerRigidBody.velocity.y <= 0)
-        {
-            playerAnim.SetBool("Going_up", false);
-        }
-        //constant forward movement
-        if (jumping==false)
-        {
-            transform.Translate(movement * speed * Time.deltaTime);
-        }
-        //jump
-        if (Input.GetKeyDown(KeyCode.Space)&&jumping==false && flying == false)
-        {
-            playerAnim.SetTrigger("Jump_start");
-            jumping = true;
-            Debug.Log("Hypättiin");
-            PlayerRigidBody.AddForce(Vector3.up * jumpForceUp, ForceMode.Impulse);
-            PlayerRigidBody.AddForce(Vector3.forward * jumpForceFwd, ForceMode.Impulse);
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            playerAnim.SetTrigger("Strafe");
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            playerAnim.SetTrigger("Strafe_Left");
-        }
-        //slide
-        if (Input.GetKeyDown("s") && jumping == false && flying == false)
-        {
-            Debug.Log("slide");
-            StartCoroutine(Slide());
-        }
-        if (hawkPicked == true)
-        {
-            if (flying == false)
+            while (transform.position.y < flyPos.y)
             {
-                StartCoroutine(Fly());
-                hawkPicked = false;
+                transform.Translate(Vector3.up * takeOffSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
             }
-        }
-        //speed increase
-        speed += Time.deltaTime * incrementGrowth;
-    }
+            yield return new WaitForSeconds(flyTime);
+            landPos = new Vector3(transform.position.x, transform.position.y - flyHeight, transform.position.z);
+            while (transform.position.y > landPos.y)
+            {
+                transform.Translate(Vector3.down * takeOffSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+            PlayerRigidBody.useGravity = true;
+            flying = false;
 
+
+        }
+
+
+        IEnumerator GameOver()
+        {
+            yield return new WaitForSeconds(exitTime);
+            SceneManager.LoadScene(1);
+
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            movement = new Vector3(0, 0, 1);
+            PlayerRigidBody = PlayerBody.GetComponent<Rigidbody>();
+            playerColl = PlayerBody.GetComponent<PlayerCollisionDetection>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (PlayerRigidBody.velocity.y >= 0)
+            {
+                playerAnim.SetBool("Going_up", true);
+            }
+            if (PlayerRigidBody.velocity.y <= 0)
+            {
+                playerAnim.SetBool("Going_up", false);
+            }
+            //constant forward movement
+            if (jumping == false)
+            {
+                transform.Translate(movement * speed * Time.deltaTime);
+            }
+            //jump
+            if (Input.GetKeyDown(KeyCode.Space) && jumping == false && flying == false)
+            {
+                playerAnim.SetTrigger("Jump_start");
+                jumping = true;
+                Debug.Log("Hypättiin");
+                PlayerRigidBody.AddForce(Vector3.up * jumpForceUp, ForceMode.Impulse);
+                PlayerRigidBody.AddForce(Vector3.forward * jumpForceFwd, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown("d"))
+            {
+                playerAnim.SetTrigger("Strafe");
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                playerAnim.SetTrigger("Strafe_Left");
+            }
+            //slide
+            if (Input.GetKeyDown("s") && jumping == false && flying == false)
+            {
+                Debug.Log("slide");
+                StartCoroutine(Slide());
+            }
+            if (hawkPicked == true)
+            {
+                if (flying == false)
+                {
+                    StartCoroutine(Fly());
+                    hawkPicked = false;
+                }
+            }
+            //speed increase
+            speed += Time.deltaTime * incrementGrowth;
+        }
+    
 
 
     //functions for player collisions
