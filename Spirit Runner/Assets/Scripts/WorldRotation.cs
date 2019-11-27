c0e0416ae38a51cf1285fba98fd5bc9a0c0f0cfe
JoelMarttala <jokke.marttala@gmail.com>
2019-11-27 12:54:37 +0200

    Updates
    Map generator and bug fixes

--- Spirit Runner/Assets/Scripts/WorldRotation.cs
+++ Spirit Runner/Assets/Scripts/WorldRotation.cs
@@ -17,11 +17,11 @@ public class WorldRotation : MonoBehaviour
         //sideways movement by rotating the level
         if (Input.GetKeyDown("d"))
         {
-            transform.Rotate(0, -rotateAngle,0);
+            transform.Rotate(0,-rotateAngle,0);
         }
         if (Input.GetKeyDown("a"))
         {
-            transform.Rotate(0, rotateAngle,0);
+           transform.Rotate(0,rotateAngle,0);
         }
     }
  
